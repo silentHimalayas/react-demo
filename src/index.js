9bb98b9e162c392344c3b6efa9677dd56eda17d1
wangyanxin <wangyanxin@xiaomi.com>
2019-09-20 10:33:04 +0800

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import willLeave from './views/willLeave/index'
 
        
 
-// 引入全asdasda局组件12123123  main1
+// 引入全asdasda局组件12123123  main1as
 import { SideBar } from '@/layoutComponents';
 const MyContext = React.createContext(1);
 
